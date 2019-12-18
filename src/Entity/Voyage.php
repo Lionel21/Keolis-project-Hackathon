1af5934201fbb06c53ecca713fb34fd2d532ee57
korede lacici <lafical@yahoo.fr>
2019-12-18 20:20:42 +0100

    user default

---
+++ src/Entity/Voyage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VoyageRepository")
+ */
+class Voyage
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $distance;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDistance(): ?float
+    {
+        return $this->distance;
+    }
+
+    public function setDistance(float $distance): self
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+}
