3b7dfcfa09f57327480676fdc24cd2dbfbf75bd5
sdesousa <sylvaindesousa@free.fr>
2019-12-18 17:56:15 +0100

    homeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Services\StationsService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_index")
+     */
+    public function index(StationsService $stationsService): Response
+    {
+
+        $stations = $stationsService->getStations();
+
+        var_dump($stations);
+        die();
+        return $this->render('/home/index.html.twig', [
+            'stations' => $stations,
+        ]);
+    }
+}
---
+++ src/Services/StationsService.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Services;
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class StationsService
+{
+    public function getStations(): array
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://data.orleans-metropole.fr/api/records/1.0/search/?dataset=liste-des-stations-velo-2018-orleans-metropole&rows=35');
+        $content = $response->toArray();
+
+        foreach ($content['records'] as $station) {
+            $stations = $content['records'];
+        }
+
+        return $stations;
+    }
+}
