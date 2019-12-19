5db568f6991496fa71009577979abda8c4815617
Steven Bédu <steven.bedu@gmail.com>
2019-12-19 17:43:17 +0100

    fix cal

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\CalorieService;
 
 
 class HomeController extends AbstractController
