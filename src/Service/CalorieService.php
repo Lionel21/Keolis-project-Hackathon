07e1ee03a0a8199c2c21f1e47e1b90be9dbacbf8
Steven Bédu <steven.bedu@gmail.com>
2019-12-20 10:38:28 +0100

    dev

--- src/Service/CalorieService.php
+++ src/Service/CalorieService.php
@@ -3,6 +3,10 @@
 
 namespace App\Service;
 
+use App\Entity\Travel;
+use App\Entity\User;
+use App\Repository\VoyageRepository;
+
 class CalorieService
 {
     const  KCALMIN =  24.5/200;
@@ -10,5 +14,15 @@ class CalorieService
     {
         $calorie = self::KCALMIN*$weight*($duration/60);
         return $calorie;
+    }
+
+    public function getKcalTotal(VoyageRepository $travelRepository, User $user): float
+    {
+        $travels = $travelRepository->findBy(['user' => $user]);
+        $total = 0;
+        foreach ($travels as $travel) {
+            $total += $travel->getCalory();
+        }
+        return $total;
     }
 }
